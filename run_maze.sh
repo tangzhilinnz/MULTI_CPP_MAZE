ad43ecd2bea08338fc2c524c7dfe2391d191e5f0
tangzhilin <48879027+tangzhilinnz@users.noreply.github.com>
2025-12-06 16:22:30 -0600

    Refactor run_maze.sh to use an array of files

--- run_maze.sh
+++ run_maze.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-echo "Running MazeSolver for all .data files..."
+echo "Running MazeSolver"
 
-for file in *.data
+files=(
+    "Maze1Kx1K.data"
+    "Maze2Kx2K.data"
+    "Maze5Kx5K.data"
+    "Maze10Kx10K.data"
+    "Maze15Kx15K.data"
+    "Maze15Kx15K_E.data"
+    "Maze15Kx15K_J.data"
+    "Maze20Kx20K_B.data"
+    "Maze20Kx20K_D.data"
+)
+
+for f in "${files[@]}"
 do
-    echo "Running MazeSolver on $file ..."
-        ./MazeSolver "$file"
-        done
+    if [[ -f "$f" ]]; then
+        echo "Running MazeSolver on $f ..."
+        ./MazeSolver "$f"
+    else
+        echo "File not found: $f (skipping)"
+    fi
+done
 
 echo "Finished!"
