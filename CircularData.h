cdc2779cd63f1c4b8a7653d486d4fcf303289942
Tang Zhilin <tangzhilinnz@gmail.com>
2025-12-06 14:59:52 -0600

    refactor the circular queue to lock free style

--- CircularData.h
+++ CircularData.h
@@ -1,51 +1,98 @@
+//#ifndef CIRCULAR_DATA_H
+//#define CIRCULAR_DATA_H
+//
+//#include "Position.h"
+//#include <mutex>
+//
+//class CircularData
+//{
+//private:
+//	class CircularIndex
+//	{
+//	public:
+//		CircularIndex(unsigned int buffSize);
+//
+//		CircularIndex() = delete;
+//		CircularIndex(const CircularIndex &) = delete;
+//		const CircularIndex &operator = (const CircularIndex &) = delete;
+//		~CircularIndex() = default;
+//
+//		// postfix
+//		unsigned int operator++(int);
+//		bool operator==(const CircularIndex &tmp);
+//		bool operator!=(const CircularIndex &tmp);
+//
+//		// accessor
+//		unsigned int Index() const;
+//
+//	private:
+//		unsigned int index;
+//		unsigned int size;
+//		unsigned int mask;
+//	};
+//
+//public:
+//	// Needs to be a power of 2
+//	static const int CIRCULAR_DATA_SIZE = 1 << 13;
+//
+//	// verify that buffSize is a power of 2
+//	static_assert((CIRCULAR_DATA_SIZE &(CIRCULAR_DATA_SIZE - 1)) == 0, "not Power of 2");
+//
+//public:
+//	CircularData();
+//
+//	CircularData(const CircularData &) = delete;
+//	const CircularData &operator = (const CircularData &) = delete;
+//	~CircularData() = default;
+//
+//	bool PushBack(Position pPos);
+//	bool PopFront(Position& pPos);
+//
+//	bool IsEmpty();
+//
+//private:
+//	Position data[CIRCULAR_DATA_SIZE];
+//
+//	CircularIndex front;
+//	CircularIndex back;
+//	bool empty;
+//	bool full;
+//	char pad[6];
+//	std::mutex mtx;
+//};
+//
+//#endif
+//
+////---  End of File ---
+
 #ifndef CIRCULAR_DATA_H
 #define CIRCULAR_DATA_H
 
 #include "Position.h"
-#include <mutex>
+#include <atomic>
+#include <cassert>
 
 class CircularData
 {
-private:
-	class CircularIndex
-	{
-	public:
-		CircularIndex(unsigned int buffSize);
-
-		CircularIndex() = delete;
-		CircularIndex(const CircularIndex &) = delete;
-		const CircularIndex &operator = (const CircularIndex &) = delete;
-		~CircularIndex() = default;
-
-		// postfix
-		unsigned int operator++(int);
-		bool operator==(const CircularIndex &tmp);
-		bool operator!=(const CircularIndex &tmp);
-
-		// accessor
-		unsigned int Index() const;
-
-	private:
-		unsigned int index;
-		unsigned int size;
-		unsigned int mask;
-	};
-
 public:
 	// Needs to be a power of 2
 	static const int CIRCULAR_DATA_SIZE = 1 << 13;
+	static const int MASK = CIRCULAR_DATA_SIZE - 1;
 
 	// verify that buffSize is a power of 2
-	static_assert((CIRCULAR_DATA_SIZE &(CIRCULAR_DATA_SIZE - 1)) == 0, "not Power of 2");
+	static_assert((CIRCULAR_DATA_SIZE& (CIRCULAR_DATA_SIZE - 1)) == 0, "not Power of 2");
 
 public:
 	CircularData();
 
-	CircularData(const CircularData &) = delete;
-	const CircularData &operator = (const CircularData &) = delete;
+	CircularData(const CircularData&) = delete;
+	const CircularData& operator = (const CircularData&) = delete;
 	~CircularData() = default;
 
+	// Producer calls this
 	bool PushBack(Position pPos);
+
+	// Consumer calls this
 	bool PopFront(Position& pPos);
 
 	bool IsEmpty();
@@ -53,14 +100,13 @@ public:
 private:
 	Position data[CIRCULAR_DATA_SIZE];
 
-	CircularIndex front;
-	CircularIndex back;
-	bool empty;
-	bool full;
-	char pad[6];
-	std::mutex mtx;
+	// CACHE LINE PADDING
+	// Aligning these to separate cache lines (64 bytes) prevents false sharing
+	// between the producer thread writing 'back' and consumer writing 'front'.
+	alignas(64) std::atomic<unsigned int> front;
+	alignas(64) std::atomic<unsigned int> back;
+
+	char pad[64]; // Extra safety padding at the end
 };
 
 #endif
-
-//---  End of File ---
