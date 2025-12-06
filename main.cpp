d98b4fcbf7810e4b3b43c75f0869b727c9878635
tangzhilin <48879027+tangzhilinnz@users.noreply.github.com>
2025-12-06 16:08:49 -0600

    Refactor main.cpp to improve readability and structure

--- main.cpp
+++ main.cpp
@@ -171,12 +171,13 @@ int main(int argc, char *argv[])
 		printf("Results (%s):\n\n",inFileName);
 		printf("   BFS      : %f s\n",ST_BFSTime);
 		printf("   DFS      : %f s\n",ST_DFSTime);
-		printf("   Student  : %f s\n", MT_StudentTime_1);
-		printf("   Student  : %f s\n", MT_StudentTime_2);
+		printf("   MT_M1      : %f s\n", MT_StudentTime_1);
+		printf("   MT_M2      : %f s\n", MT_StudentTime_2);
 
 		printf("\nMaze: end() --------------\n\n");
 
 }
 
 // --- End of File ----
+
 
