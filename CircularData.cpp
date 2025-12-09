aa9af4c37f937365b3072019eed2323231b90476
Tang Zhilin <tangzhilinnz@gmail.com>
2025-12-08 21:53:53 -0600

    Returned to mutex circular queue

--- CircularData.cpp
+++ CircularData.cpp
@@ -2,209 +2,123 @@
 // Copyright 2025, Ed Keenan, all rights reserved.
 //----------------------------------------------------------------------------- 
 
-//#include "CircularData.h"
-//#include <assert.h>
-//#include <cstdio>
-//#include <cstring>
-//
-//bool CircularData::IsEmpty()
-//{
-//	return this->empty;
-//}
-//
-//CircularData::CircularData()
-//	: front(CIRCULAR_DATA_SIZE),
-//	back(CIRCULAR_DATA_SIZE),
-//	empty(true),
-//	full(false)
-//{
-//}
-//
-//bool CircularData::PushBack(Position pPos)
-//{
-//	std::lock_guard<std::mutex> lock(this->mtx);
-//
-//	bool status = true;
-//
-//	// Do we have space to add?
-//	if(this->front != this->back || this->empty)
-//	{
-//		// add data
-//		this->data[this->back.Index()] = pPos;
-//		this->back++;
-//
-//		this->empty = false;
-//
-//		// check if this add made it full
-//		if(this->front == this->back)
-//		{
-//			this->full = true;
-//			printf("full ===================\n");
-//			// Safety
-//			assert(false);
-//		}
-//	}
-//	else
-//	{
-//		status = false;
-//	}
-//
-//	return status;
-//}
-//
-//bool CircularData::PopFront(Position& pPos)
-//{
-//	std::lock_guard<std::mutex> lock(this->mtx);
-//
-//	bool status = true;
-//
-//	// Is there data to process?
-//	if(this->front != this->back || this->full)
-//	{
-//		// Grab one
-//		pPos = this->data[this->front.Index()];
-//		this->front++;
-//
-//		this->full = false;
-//
-//		// check if this Pop made it Empty
-//		if(this->front == this->back)
-//		{
-//			this->empty = true;
-//		}
-//	}
-//	else
-//	{
-//		status = false;
-//	}
-//	return status;
-//}
-//
-//
-//CircularData::CircularIndex::CircularIndex(unsigned int buffSize)
-//{
-//
-//	this->size = buffSize;
-//	this->mask = (unsigned int)(buffSize - 1);
-//
-//	// verify that buffSize is a power of 2
-//	assert((size & (size - 1)) == 0);
-//
-//	this->index = 0;
-//}
-//
-//// postfix
-//unsigned int CircularData::CircularIndex::operator++(int)
-//{
-//	this->index++;
-//
-//	// Circular 
-//	this->index = this->index & this->mask;
-//
-//	return this->index;
-//}
-//
-//bool CircularData::CircularIndex::operator==(const CircularIndex &tmp)
-//{
-//	assert(this->size == tmp.size);
-//	return (this->index == tmp.index);
-//}
-//
-//bool CircularData::CircularIndex::operator!=(const CircularIndex &tmp)
-//{
-//	assert(this->size == tmp.size);
-//	return (this->index != tmp.index);
-//}
-//
-//unsigned int CircularData::CircularIndex::Index() const
-//{
-//	return this->index;
-//}
-////---  End of File ---
-
-
-//-----------------------------------------------------------------------------
-// Copyright 2025, Ed Keenan, all rights reserved.
-//----------------------------------------------------------------------------- 
-
 #include "CircularData.h"
+#include <assert.h>
+#include <cstdio>
+#include <cstring>
 
-CircularData::CircularData()
-    : head(0),
-    tail(0)
+bool CircularData::IsEmpty()
 {
+	return this->empty;
 }
 
-bool CircularData::IsEmpty() const
+CircularData::CircularData()
+	: front(CIRCULAR_DATA_SIZE),
+	back(CIRCULAR_DATA_SIZE),
+	empty(true),
+	full(false)
 {
-    // If head and tail match, there is no data
-    // using Relaxed because this is usually just a state check, 
-    // strictly speaking, Acquire is safer if you act on it immediately externally.
-    return (this->head.load(std::memory_order_relaxed) == this->tail.load(std::memory_order_relaxed));
 }
 
 bool CircularData::PushBack(Position pPos)
 {
-    // 1. Load the current Write Index (Relaxed is fine, only WE change it)
-    const auto currentHead = this->head.load(std::memory_order_relaxed);
+	std::lock_guard<std::mutex> lock(this->mtx);
 
-    // 2. Calculate next Write Index
-    const auto nextHead = (currentHead + 1) & MASK;
+	bool status = true;
 
-    // 3. Load the Read Index (Acquire)
-    // We need Acquire here to ensure that any reads of the slot we are about to 
-    // write to (by the consumer) are finished before we overwrite it.
-    const auto currentTail = this->tail.load(std::memory_order_acquire);
+	// Do we have space to add?
+	if(this->front != this->back || this->empty)
+	{
+		// add data
+		this->data[this->back.Index()] = pPos;
+		this->back++;
 
-    // 4. Check if Full (One Slot Empty Rule)
-    if (nextHead == currentTail)
-    {
-        // Queue is full
-        return false;
-    }
+		this->empty = false;
 
-    // 5. Write Data
-    // We are safe to write because we know the consumer isn't reading this slot (Step 4)
-    this->data[currentHead] = pPos;
+		// check if this add made it full
+		if(this->front == this->back)
+		{
+			this->full = true;
+			printf("full ===================\n");
+			// Safety
+			assert(false);
+		}
+	}
+	else
+	{
+		status = false;
+	}
 
-    // 6. Update Write Index (Release)
-    // We use Release to ensure the write to 'data' (Step 5) is visible to the 
-    // consumer BEFORE they see the new head index.
-    this->head.store(nextHead, std::memory_order_release);
-
-    return true;
+	return status;
 }
 
 bool CircularData::PopFront(Position& pPos)
 {
-    // 1. Load the current Read Index (Relaxed is fine, only WE change it)
-    const auto currentTail = this->tail.load(std::memory_order_relaxed);
+	std::lock_guard<std::mutex> lock(this->mtx);
 
-    // 2. Load the Write Index (Acquire)
-    // We need Acquire here to ensure that the data written by the producer 
-    // is fully visible to us before we try to read it.
-    const auto currentHead = this->head.load(std::memory_order_acquire);
+	bool status = true;
 
-    // 3. Check if Empty
-    if (currentTail == currentHead)
-    {
-        // Queue is empty
-        return false;
-    }
+	// Is there data to process?
+	if(this->front != this->back || this->full)
+	{
+		// Grab one
+		pPos = this->data[this->front.Index()];
+		this->front++;
 
-    // 4. Read Data
-    pPos = this->data[currentTail];
+		this->full = false;
 
-    // 5. Calculate next Read Index
-    const auto nextTail = (currentTail + 1) & MASK;
-
-    // 6. Update Read Index (Release)
-    // We use Release so the producer knows we are done with this slot 
-    // and they can safely overwrite it in the future.
-    this->tail.store(nextTail, std::memory_order_release);
-
-    return true;
+		// check if this Pop made it Empty
+		if(this->front == this->back)
+		{
+			this->empty = true;
+		}
+	}
+	else
+	{
+		status = false;
+	}
+	return status;
 }
 
+
+CircularData::CircularIndex::CircularIndex(unsigned int buffSize)
+{
+
+	this->size = buffSize;
+	this->mask = (unsigned int)(buffSize - 1);
+
+	// verify that buffSize is a power of 2
+	assert((size & (size - 1)) == 0);
+
+	this->index = 0;
+}
+
+// postfix
+unsigned int CircularData::CircularIndex::operator++(int)
+{
+	this->index++;
+
+	// Circular 
+	this->index = this->index & this->mask;
+
+	return this->index;
+}
+
+bool CircularData::CircularIndex::operator==(const CircularIndex &tmp)
+{
+	assert(this->size == tmp.size);
+	return (this->index == tmp.index);
+}
+
+bool CircularData::CircularIndex::operator!=(const CircularIndex &tmp)
+{
+	assert(this->size == tmp.size);
+	return (this->index != tmp.index);
+}
+
+unsigned int CircularData::CircularIndex::Index() const
+{
+	return this->index;
+}
 //---  End of File ---
 
--- CircularData.h
+++ CircularData.h
@@ -1,117 +1,66 @@
-//#ifndef CIRCULAR_DATA_H
-//#define CIRCULAR_DATA_H
-//
-//#include "Position.h"
-//#include <mutex>
-//
-//class CircularData
-//{
-//private:
-//	class CircularIndex
-//	{
-//	public:
-//		CircularIndex(unsigned int buffSize);
-//
-//		CircularIndex() = delete;
-//		CircularIndex(const CircularIndex &) = delete;
-//		const CircularIndex &operator = (const CircularIndex &) = delete;
-//		~CircularIndex() = default;
-//
-//		// postfix
-//		unsigned int operator++(int);
-//		bool operator==(const CircularIndex &tmp);
-//		bool operator!=(const CircularIndex &tmp);
-//
-//		// accessor
-//		unsigned int Index() const;
-//
-//	private:
-//		unsigned int index;
-//		unsigned int size;
-//		unsigned int mask;
-//	};
-//
-//public:
-//	// Needs to be a power of 2
-//	static const int CIRCULAR_DATA_SIZE = 1 << 13;
-//
-//	// verify that buffSize is a power of 2
-//	static_assert((CIRCULAR_DATA_SIZE &(CIRCULAR_DATA_SIZE - 1)) == 0, "not Power of 2");
-//
-//public:
-//	CircularData();
-//
-//	CircularData(const CircularData &) = delete;
-//	const CircularData &operator = (const CircularData &) = delete;
-//	~CircularData() = default;
-//
-//	bool PushBack(Position pPos);
-//	bool PopFront(Position& pPos);
-//
-//	bool IsEmpty();
-//
-//private:
-//	Position data[CIRCULAR_DATA_SIZE];
-//
-//	CircularIndex front;
-//	CircularIndex back;
-//	bool empty;
-//	bool full;
-//	char pad[6];
-//	std::mutex mtx;
-//};
-//
-//#endif
-//
-////---  End of File ---
-
-
-
 #ifndef CIRCULAR_DATA_H
 #define CIRCULAR_DATA_H
 
 #include "Position.h"
-#include <atomic>
-#include <cstddef>
+#include <mutex>
 
 class CircularData
 {
+private:
+	class CircularIndex
+	{
+	public:
+		CircularIndex(unsigned int buffSize);
+
+		CircularIndex() = delete;
+		CircularIndex(const CircularIndex &) = delete;
+		const CircularIndex &operator = (const CircularIndex &) = delete;
+		~CircularIndex() = default;
+
+		// postfix
+		unsigned int operator++(int);
+		bool operator==(const CircularIndex &tmp);
+		bool operator!=(const CircularIndex &tmp);
+
+		// accessor
+		unsigned int Index() const;
+
+	private:
+		unsigned int index;
+		unsigned int size;
+		unsigned int mask;
+	};
+
 public:
-    // Needs to be a power of 2
-    static const int CIRCULAR_DATA_SIZE = 1 << 13;
+	// Needs to be a power of 2
+	static const int CIRCULAR_DATA_SIZE = 1 << 13;
 
-    // verify that buffSize is a power of 2
-    static_assert((CIRCULAR_DATA_SIZE& (CIRCULAR_DATA_SIZE - 1)) == 0, "not Power of 2");
+	// verify that buffSize is a power of 2
+	static_assert((CIRCULAR_DATA_SIZE &(CIRCULAR_DATA_SIZE - 1)) == 0, "not Power of 2");
 
 public:
-    CircularData();
+	CircularData();
 
-    CircularData(const CircularData&) = delete;
-    const CircularData& operator = (const CircularData&) = delete;
-    ~CircularData() = default;
+	CircularData(const CircularData &) = delete;
+	const CircularData &operator = (const CircularData &) = delete;
+	~CircularData() = default;
 
-    // Producer calls this
-    bool PushBack(Position pPos);
+	bool PushBack(Position pPos);
+	bool PopFront(Position& pPos);
 
-    // Consumer calls this
-    bool PopFront(Position& pPos);
-
-    bool IsEmpty() const;
+	bool IsEmpty();
 
 private:
-    // Mask for quick modulus operations (size - 1)
-    static const unsigned int MASK = CIRCULAR_DATA_SIZE - 1;
+	Position data[CIRCULAR_DATA_SIZE];
 
-    // The actual data buffer
-    Position data[CIRCULAR_DATA_SIZE];
-
-    // Align to 64 bytes (typical cache line size) to prevent False Sharing
-    // between the Producer thread (modifies head) and Consumer thread (modifies tail).
-    alignas(64) std::atomic<unsigned int> head; // Write Index (Back)
-    char pad1[60];
-    alignas(64) std::atomic<unsigned int> tail; // Read Index (Front)
-    char pad2[60];
+	CircularIndex front;
+	CircularIndex back;
+	bool empty;
+	bool full;
+	char pad[6];
+	std::mutex mtx;
 };
 
 #endif
+
 //---  End of File ---
