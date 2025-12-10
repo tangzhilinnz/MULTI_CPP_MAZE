a9cc31f8a1dea1bf1dbe008b1d7623a734684c8f
Tang Zhilin <tangzhilinnz@gmail.com>
2025-12-10 12:53:18 -0600

    Added walkThread_BFS_TB to Multi-threaded solver

--- MTMazeStudentSolver.h
+++ MTMazeStudentSolver.h
@@ -29,6 +29,63 @@ public:
 	MTMazeStudentSolver &operator=(const MTMazeStudentSolver &) = delete;
 	~MTMazeStudentSolver() = default;
 
+	void setDirectionRouteTB(Position at, Direction from)
+	{
+		unsigned int val = pMaze->getCell(at);
+		int dir = 0;
+
+		switch (from)
+		{
+		case Direction::East:
+			dir = 0x10000000;
+			break;
+		case Direction::West:
+			dir = 0x20000000;
+			break;
+		case Direction::South:
+			dir = 0x40000000;
+			break;
+		case Direction::North:
+			dir = 0x80000000;
+			break;
+		case Direction::Uninitialized:
+		default:
+			assert(0);
+			break;
+		}
+
+		val |= dir;
+		pMaze->setCell(at, val);
+	}
+
+	Direction getDirectionRouteTB(Position at)
+	{
+		Direction dir = Direction::Uninitialized;
+		unsigned int val = pMaze->getCell(at);
+		val &= 0xF0000000;
+
+		switch (val)
+		{
+		case 0x10000000:
+			dir = Direction::East;
+			break;
+		case 0x20000000:
+			dir = Direction::West;
+			break;
+		case 0x40000000:
+			dir = Direction::South;
+			break;
+		case 0x80000000:
+			dir = Direction::North;
+			break;
+		default:
+			assert(0);
+			break;
+		}
+
+		return dir;
+	}
+
 	void setDirectionRouteBT(Position at, Direction from)
 	{
 		unsigned int val = pMaze->getCell(at);
@@ -539,6 +596,129 @@ public:
 		}
 	}
 
+	void walkThread_BFS_TB(std::vector<Direction>*& pSolve, bool& exit, bool& firstExit)
+	{
+		Position start = pMaze->getStart();
+		Position end = pMaze->getEnd();
+		std::queue<Position> q;
+		q.push(start);
+		Position overlap;
+
+		// Mark start route (no parent)
+		setDirectionRouteTB(start, Direction::Uninitialized);
+
+		while (!q.empty())
+		{
+			Position cur = q.front();
+			q.pop();
+
+			if (getDirectionRouteBT(cur) != Direction::Uninitialized)
+			{
+				overlap = cur;
+				break;
+			}
+
+			if (cur == end)
+			{
+				break;
+			}
+
+			if (pMaze->isDeadCell(cur))
+			{
+				continue;
+			}
+
+			Direction came_from = getDirectionRouteTB(cur);
+
+			if (came_from != Direction::South && pMaze->canMove(cur, Direction::South))
+			{
+				Position nextPos = cur.move(Direction::South);
+
+				if (getDirectionRouteTB(nextPos) == Direction::Uninitialized)
+				{
+					// Mark parent
+					setDirectionRouteTB(nextPos, Direction::North);
+					q.push(nextPos);
+				}
+			}
+
+			if (came_from != Direction::West && pMaze->canMove(cur, Direction::West))
+			{
+				Position nextPos = cur.move(Direction::West);
+
+				if (getDirectionRouteTB(nextPos) == Direction::Uninitialized)
+				{
+					// Mark parent
+					setDirectionRouteTB(nextPos, Direction::East);
+					q.push(nextPos);
+				}
+			}
+
+			if (came_from != Direction::East && pMaze->canMove(cur, Direction::East))
+			{
+				Position nextPos = cur.move(Direction::East);
+
+				if (getDirectionRouteTB(nextPos) == Direction::Uninitialized)
+				{
+					// Mark parent
+					setDirectionRouteTB(nextPos, Direction::West);
+					q.push(nextPos);
+				}
+			}
+
+			if (came_from != Direction::North && pMaze->canMove(cur, Direction::North))
+			{
+				Position nextPos = cur.move(Direction::North);
+
+				if (getDirectionRouteTB(nextPos) == Direction::Uninitialized)
+				{
+					// Mark parent
+					setDirectionRouteTB(nextPos, Direction::South);
+					q.push(nextPos);
+				}
+			}
+
+			std::this_thread::yield();
+		}
+
+		exit = true;
+
+		pSolve->reserve(VECTOR_RESERVE_SIZE);
+
+		// Reconstruct path
+		if (firstExit)
+		{
+			Position curr = pMaze->getStart();
+			pSolve->clear();
+			while (curr != end)
+			{
+				Direction dir = getDirectionRouteBT(curr);
+				pSolve->push_back(dir);
+				curr = curr.move(dir);
+			}
+		}
+		else
+		{
+			Position curr = overlap;
+			while (curr != start)
+			{
+				Direction dir = getDirectionRouteTB(curr);
+				pSolve->push_back(reverseDir(dir));
+				curr = curr.move(dir);
+			}
+
+			std::reverse(pSolve->begin(), pSolve->end());
+
+			curr = overlap;
+			while (curr != end)
+			{
+				Direction dir = getDirectionRouteBT(curr);
+				pSolve->push_back(dir);
+				curr = curr.move(dir);
+			}
+		}
+	}
+
 	void walkThread_BFS_BT(bool& exit, bool& firstExit)
 	{
 		Position start = pMaze->getEnd();
@@ -1000,7 +1180,8 @@ public:
 		
 		std::thread walkReverse(&MTMazeStudentSolver::walkThread_BFS_BT, this, std::ref(exit), std::ref(firstExit));
 
-		this->walkThreadTB(pTB, exit, firstExit);
+		//this->walkThreadTB(pTB, exit, firstExit);
+		this->walkThread_BFS_TB(pTB, exit, firstExit);
 
 		headThread.join();
 		tailThread.join();
@@ -1069,7 +1250,7 @@ public:
 	std::vector<Direction>* Solve_1()
 	{
 		std::vector<Direction>* pTB = new std::vector<Direction>();
-		this->StartParallelPruning_Method1(168, pTB);
+		this->StartParallelPruning_Method1(180, pTB);
 		return pTB;
 	}
 
